d555593bbfa66e9981c56ee45203bdbf0bc09c1d
RDK7159357 <155869647+RDK7159357@users.noreply.github.com>
2024-09-26 20:29:08 +0530

    24

---
+++ Q55.java
@@ -0,0 +1,15 @@
+public class Q55 {
+    public static void main(String[] args) {
+        int arr[]={2,3,1,1,4};
+        System.out.println(canJump(arr));
+    }
+    public static boolean canJump(int[] nums) {
+        int goal =nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=goal){
+                goal=i;
+            }
+        }
+        return goal ==0;
+    }
+}
